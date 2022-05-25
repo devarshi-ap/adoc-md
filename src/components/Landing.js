b986119835db4551a2a74fe9c0afebd23028c17f
Dev Patel <devarshi.ap@gmail.com>
2022-05-25 19:25:23 -0400

    remove warning text

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -6,9 +6,8 @@ export default function LandingSection() {
             <div className='card'>
                 <div className='content'>
                     <h1 className={styles.title}>Markup-Convert <div className="rotate">&#129422;</div></h1>
-                    <h4 className={styles.subtitle}>Transform Text to AsciiDocs (.adoc) or Markdown (.md)</h4>
+                    <h4 className={styles.subtitle}>Unidirectional Conversion: AsciiDoc || Markdown -{">"} HTML </h4>
                     <p className={styles.description}>Experience the magic of a lightweight markup text editor!</p>
-                    <p className={styles.warning}>Warning: conversion is not bidirectional</p>
                 </div>
             </div>
         </div>
